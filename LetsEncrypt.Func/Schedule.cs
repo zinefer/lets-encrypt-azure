aca0c5662f1b115950a825bae81ad298b918488f
Marc Stan <marcstan@live.com>
2019-09-02 20:20:40 +0200

    nuget update & offset schedule

--- LetsEncrypt.Func/Schedule.cs
+++ LetsEncrypt.Func/Schedule.cs
@@ -2,6 +2,9 @@
 {
     public static class Schedule
     {
-        public const string Daily = "0 0 0 * * *";
+        /// <summary>
+        /// Run daily but not at midnight to prevent service DoS as per letsencrypt guidelines.
+        /// </summary>
+        public const string Daily = "0 51 11 * * *";
     }
 }
